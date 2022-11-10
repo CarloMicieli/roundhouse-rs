368493746b3fc6766b84ce5052ddcfd6140a6ede
CarloMicieli <carlo.micieli@gmail.com>
2022-11-10 07:03:34 +0100

    feat(common): more work on the Length type

--- crates/common/src/length.rs
+++ crates/common/src/length.rs
@@ -1,6 +1,7 @@
 use crate::measure_units::MeasureUnit;
-use rust_decimal::prelude::FromPrimitive;
+use rust_decimal::prelude::{FromPrimitive, Zero};
 use rust_decimal::Decimal;
+use std::cmp;
 use std::fmt;
 use std::fmt::Formatter;
 use std::ops;
@@ -57,6 +58,12 @@ impl Length {
     }
 }
 
+impl Default for Length {
+    fn default() -> Self {
+        Length::Millimeters(Decimal::zero())
+    }
+}
+
 impl fmt::Display for Length {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{} {}", self.quantity(), self.measure_unit().symbol())
@@ -69,9 +76,28 @@ impl ops::Add for Length {
     fn add(self, rhs: Self) -> Self::Output {
         let (val1, mu1) = (self.quantity(), self.measure_unit());
         let (val2, mu2) = (rhs.quantity(), rhs.measure_unit());
-        Length::create(val1 + val2, self.measure_unit()).unwrap()
+
+        let new_value = val1 + mu2.to(mu1).convert(val2);
+
+        Length::create(new_value, self.measure_unit()).unwrap()
     }
 }
+
+impl cmp::PartialEq for Length {
+    fn eq(&self, other: &Self) -> bool {
+        let (val1, mu1) = (self.quantity(), self.measure_unit());
+        let (val2, mu2) = (other.quantity(), other.measure_unit());
+
+        if mu1 == mu2 {
+            val1 == val2
+        } else {
+            let val2_converted = mu2.to(mu1).convert(val2);
+            val1 == val2_converted
+        }
+    }
+}
+
+impl cmp::Eq for Length {}
 
 #[cfg(test)]
 mod test {
@@ -90,6 +116,15 @@ mod test {
         }
 
         #[test]
+        fn it_should_ensure_lengths_are_non_negative() {
+            assert_eq!(None, Length::new(-1_f32, MeasureUnit::Inches));
+            assert_eq!(
+                Some(Length::default()),
+                Length::new(0_f32, MeasureUnit::Millimeters)
+            );
+        }
+
+        #[test]
         fn it_should_display_lengths() {
             let l = Length::new(42_f32, MeasureUnit::Millimeters).unwrap();
             assert_eq!("42 mm", l.to_string());
@@ -103,6 +138,25 @@ mod test {
             let l = l1 + l2;
             assert_eq!(dec!(42.0), l.quantity());
             assert_eq!(MeasureUnit::Millimeters, l.measure_unit());
+        }
+
+        #[test]
+        fn it_should_sum_two_lengths_converting_measure_units() {
+            let l1 = Length::new(16.6_f32, MeasureUnit::Millimeters).unwrap();
+            let l2 = Length::new(1_f32, MeasureUnit::Inches).unwrap();
+
+            let l = l1 + l2;
+            assert_eq!(dec!(42.0), l.quantity());
+            assert_eq!(MeasureUnit::Millimeters, l.measure_unit());
+        }
+
+        #[test]
+        fn it_should_compare_two_lengths() {
+            let l1 = Length::new(20.6_f32, MeasureUnit::Millimeters).unwrap();
+            let l2 = Length::new(21.4_f32, MeasureUnit::Millimeters).unwrap();
+
+            assert_eq!(l1, l1);
+            assert_ne!(l1, l2);
         }
     }
 }
